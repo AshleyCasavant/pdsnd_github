33d0151d4b24d60bf882d2f8fd017234f8259ecf
Ashley Casavant <ashley.casavant02@gmail.com>
2025-01-06 13:14:29 -0500

    refactor: Change city input loop to reference CITY_DATA dictionary keys
    Replaces the hard coded city values in the city input while loop with a reference to
    the CITY_DATA dictionary.  Allows for cleaner code that is less prone to error and more robust
    to changes.

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -19,7 +19,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("Please input city name here: ")
-        if city.lower() in ('chicago', 'new york city', 'washington'):
+        if city.lower() in CITY_DATA.keys():
             break 
         else:
             print("Please enter a valid city")
